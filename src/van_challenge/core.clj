18bf049d469c05081c2a0de54ddfe5959a3d257d
Malcolm Stone <m.stone@kew.org>
2021-01-22 11:40:39 +0000

    implement basic game

--- src/van_challenge/core.clj
+++ src/van_challenge/core.clj
@@ -1,7 +1,73 @@
 (ns van-challenge.core
+
   (:gen-class))
 
+(defn init-answer
+
+  "Returns a number between given limits"
+
+  [min-val max-val]
+
+  (rand-nth (range min-val (+ max-val 1))))
+
+(defn print-banner
+
+  "Prints the game intro banner"
+
+  []
+
+  (println "<><><><><><><><><><><><><><><><><><><><>")
+
+  (println "Welcome to the Van Number game! The aim of the name is to guess the number.")
+
+  (println "...calculating incredibly sneaky number... done!")
+
+  (println "<><><><><><><><><><><><><><><><><><><><>")
+
+  (println))
+
+(defn parse-int [number-string]
+
+  (try (Integer/parseInt number-string)
+
+       (catch Exception e nil)))
+
+(defn game-loop
+
+  "The main game loop"
+
+  [answer]
+
+  (println "Please enter a number to take a guess!")
+
+  (def user-input (read-line))
+
+  (def user-guess (parse-int user-input))
+
+  (cond
+
+    (= user-guess nil) (game-loop answer)
+
+    (> user-guess answer) ((println "Too high!")
+
+                           (game-loop answer))
+
+    (< user-guess answer) ((println "Too low!")
+
+                           (game-loop answer))
+
+    (= user-guess answer) (printf "You did it! The correct answer is: %s%n" answer)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+
   [& args]
-  (println "Hello, World!"))
+
+  (print-banner)
+
+  (def answer (init-answer 1 100))
+
+  (printf "The answer is %s%n" answer)
+
+  (game-loop answer)
+
+  0)
---
+++ src/van_challenge/number-game.md
@@ -0,0 +1,8 @@
+ Write a game (computer program) that follows the following rules:
+ The computer chooses a number between given set limits.
+ The player is asked for repeated guesses until the the target number is guessed correctly
+ At each guess, the computer responds with whether the guess is:
+             higher than the target,
+             equal to the target,
+             less than the target,   or
+             the input was inappropriate. 
