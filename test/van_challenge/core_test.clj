261d93e6ed2aadeaa3aa7e089fc26d6a90ce7fce
Malcolm Stone <m.stone@kew.org>
2021-01-22 10:42:23 +0000

    initial commit

---
+++ test/van_challenge/core_test.clj
@@ -0,0 +1,7 @@
+(ns van-challenge.core-test
+  (:require [clojure.test :refer :all]
+            [van-challenge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
