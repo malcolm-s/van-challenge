7cf3c49272d49c1b999b38b85c66a857d01b4886
Malcolm Stone <m.stone@kew.org>
2021-02-07 14:40:40 +0000

    add string manipulation logic and tests

--- test/van_challenge/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns van-challenge.core-test
-  (:require [clojure.test :refer :all]
-            [van-challenge.core :refer :all]))
-
-(deftest a-test
-  (testing "equality operator works"
-    (is (= 1 1))))
