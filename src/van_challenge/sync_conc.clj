2e5116bf92b8a4f7b93525082708d94be488803f
Malcolm Stone <m.stone@kew.org>
2021-02-17 22:51:12 +0000

    basic but messy concurrency working

--- src/van_challenge/sync_conc.clj
+++ src/van_challenge/sync_conc.clj
@@ -1,7 +1,7 @@
 (ns van-challenge.sync-conc
   (:require [clojure.string]
             [clojure.java.io :refer [reader]]
-            [clojure.core.async :refer [chan >!! <!! >! <! go]]))
+            [clojure.core.async :refer [close! chan >!! <!! >! <! go put! take! go-loop]]))
 
 (defn count-file-lines
   [file]
@@ -13,19 +13,40 @@
 (defn sync-conc
   [file]
   (let [line-chan (chan)]
-    (do
-      (go (doseq [line (line-seq (reader file))]
-            >! line-chan line))
-      (go (while true (println <! line-chan))))))
+    ;; (doseq [line (line-seq (reader file))]
+    ;;   (printf "from reader>%s%n" line)
+    ;;   (put! line-chan line))
+    ;; (take! line-chan println)))
+    (go (doseq [line (line-seq (reader file))]
+          (printf "from reader>%s%n")
+          (close!)
+          (>! line-chan line)))
+    (go (while true (printf "from printer>%s%n" (<! line-chan))))))
+
+;; (<!! (sync-conc "README.md"))
 
 (defn main
   [args]
   (assert (= (count args) 1) "Must provide one argument <file to read>")
-  (let [in-file (first args)]
-    (sync-conc in-file)))
-        ;; line-count (count-file-lines in-file)]
+
+  (let [in-file (first args)
+        line-chan (chan)
+        done-chan (chan)]
+    (go
+      (doseq [line (line-seq (reader in-file))]
+        (>! line-chan line))
+      (close! line-chan)
+      (printf "reader:%s%n" (<! done-chan)))
+    (<!! (go-loop []
+           (if-let [line (<! line-chan)]
+             (do
+               (printf "printer:%s%n" line)
+               (recur))
+             (>! done-chan "printing finished"))))))
+
+    ;; line-count (count-file-lines in-file)]
     ;; (printf "Line count is: %s%n" line-count)))
-;; (sync-conc "README.md")
+
 
 ;; read lines
 ;; send each line to 
